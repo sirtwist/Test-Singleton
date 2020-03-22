117af94229c9f506b7ae1d68652a19549c64081a
George Roberts <groberts@moddigital.net>
2020-03-22 11:45:34 -0500

    Initial code

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using TestRazorPages.Services;
+
+namespace TestRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly TestService _testService;
+
+        public string Data
+        {
+            get
+            {
+                return _testService.GetValue();
+            }
+        }
+
+        public IndexModel(ILogger<IndexModel> logger, TestService testService)
+        {
+            _logger = logger;
+            _testService = testService;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Services/TestService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestRazorPages.Services
+{
+    public class TestService: ITestService
+    {
+        private static string _value;
+        public string GetValue()
+        {
+            return _value;
+        }
+
+        public TestService()
+        {
+            CancellationTokenSource source = new CancellationTokenSource();
+
+            var t = Task.Run(async delegate
+            {
+                // replace these two lines with your async call and returning the resulting string.
+                await Task.Delay(TimeSpan.FromSeconds(10), source.Token);
+                return "value that you derived from your async call";
+            });
+            try
+            {
+               _value = t.Result;
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var e in ae.InnerExceptions)
+                    Debug.WriteLine("{0}: {1}", e.GetType().Name, e.Message);
+            }
+            Console.Write("Task t Status: {0}", t.Status);
+            if (t.Status == TaskStatus.RanToCompletion)
+                Debug.Write(", Result: {0}", t.Result);
+            source.Dispose();
+        }
+
+    }
+}
